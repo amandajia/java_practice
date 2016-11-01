54bb46478fa1ee281022b5664021309785abb4d1
amanda <errantry_jia@hotmail.com>
2016-11-01 14:50:30 -0700

    Create 224.java

---
+++ 224.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/basic-calculator/
+  public static boolean isPalindrome(int x) {
+        if(x>=0 && x<10) return true;
+        if(x<0 || x==10) return false;
+        double length=0;
+        int temp=x;
+        while(temp>=10){ // count except the top digit, how many digits rest;
+        	length++;
+        	temp/=10;
+        }
+        int iter=((int)length+1)/2;
+        temp=x;
+        for(int i=0;i<iter;i++){
+        	int top=temp/((int)Math.pow(10, length));
+        	int tail=temp%10;
+        	if(top!=tail) return false;
+        	temp-=top*(int)Math.pow(10, length);
+        	temp=(temp-tail)/10;
+        	length-=2;
+        }
+        return true;
+  }
